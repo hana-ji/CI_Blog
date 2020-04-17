de01a8cdcd46b0954792081080b1365f44f140be
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-17 17:11:28 +0900

    부트스트렙 테마 적용

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,36 @@
 <html>
     <head>
         <title>CI_Blog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     </head>
     <body>
+<!--    <nav class="navbar navbar-inverse">-->
+<!--        <div class="container">-->
+<!--            <div class="navbar-header">-->
+<!--                <a class="navbar-brand" href="#">ciBlog</a>-->
+<!--            </div>-->
+<!--            <div id="navbar">-->
+<!--                <ul class="nav navbar-nav">-->
+<!--                    <li><a href="/">Home</a></li>-->
+<!--                    <li><a href="/about">About</a></li>-->
+<!--                </ul>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </nav>-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="/">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
